aace2ea9bef365949ceb5e43f1bfd9dfbd5caa81
kimnamhyung8 <kimnamhyung8-hash@users.noreply.github.com>
2026-04-01 04:30:01 +0700

    fix(config): add FirebaseAuthentication providers list

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -12,6 +12,10 @@ const config: CapacitorConfig = {
       // Test App IDs - Replace with real IDs for production
       androidAppId: 'ca-app-pub-6224627094460293~1345014713',
       iosAppId: 'ca-app-pub-3940256099942544~1458002511',
+    },
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ["google.com"],
     }
   }
 };
