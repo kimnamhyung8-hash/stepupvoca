6cf1d88c03d285900eaf942af08b4a08dcd0edb1
kimnamhyung8 <kimnamhyung8-hash@users.noreply.github.com>
2026-04-03 13:57:55 +0700

    Bump android version code to 77 and update aab build script

--- app/build_aab.bat
+++ app/build_aab.bat
@@ -2,6 +2,6 @@
 set "JAVA_HOME=C:\Program Files\Android\Android Studio\jbr"
 cd android
 gradlew.bat bundleRelease
-if not exist "app\release" mkdir "app\release"
-copy /y "app\build\outputs\bundle\release\app-release.aab" "app\release\app-release.aab"
 cd ..
+if not exist "release" mkdir "release"
+copy /y "android\app\build\outputs\bundle\release\app-release.aab" "release\app-release.aab"
