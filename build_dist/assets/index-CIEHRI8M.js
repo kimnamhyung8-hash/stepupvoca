caca14527f66262871bdbfa6410fc67f69b68fab
kimnamhyung8 <kimnamhyung8-hash@users.noreply.github.com>
2026-04-12 12:40:03 +0700

    feat(ai): unify all AI models to gemini-3.1-flash-lite-preview

--- build_dist/assets/index-CIEHRI8M.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Cro2W2bc.js","assets/virtual_pwa-register-Cd2Bi3bF.js","assets/virtual_pwa-register-CGzIcLSO.css"])))=>i.map(i=>d[i]);
-import{r as i,_ as p}from"./virtual_pwa-register-Cd2Bi3bF.js";const t=i("InAppReview",{web:()=>p(()=>import("./web-Cro2W2bc.js"),__vite__mapDeps([0,1,2])).then(e=>new e.InAppReviewWeb)});export{t as InAppReview};
--- build_dist/assets/web-Cro2W2bc.js
+++
@@ -1,1 +0,0 @@
-import{W as e}from"./virtual_pwa-register-Cd2Bi3bF.js";class i extends e{async requestReview(){throw this.unimplemented("Not implemented on web.")}}export{i as InAppReviewWeb};
